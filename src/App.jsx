acc9a16968b1f70b0a6b5a1c0d58d252ac122b40
Kevin Dodokal <dodokalkev@gmail.com>
2026-03-24 18:33:42 +0100

    feat: ajout Header et Footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Logement from './pages/Logement'
 import NotFound from  './pages/NotFound'
+import Header from "./components/header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element = { <Home /> }/>
         <Route path="/about" element = { <About /> }/>
         <Route path="/logement/:id" element = { <Logement /> }/>
         <Route path="*" element = { <NotFound /> }/>
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
