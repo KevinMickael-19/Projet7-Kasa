161d85d5e9103ba67b533f803b0713c04a599251
Kevin Dodokal <dodokalkev@gmail.com>
2026-03-24 18:20:37 +0100

    feat: mise en place du routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+import About from './pages/About'
+import Logement from './pages/Logement'
+import NotFound from  './pages/NotFound'
+
 function App() {
-  return <div>kasa</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element = { <Home /> }/>
+        <Route path="/about" element = { <About /> }/>
+        <Route path="/logement/:id" element = { <Logement /> }/>
+        <Route path="*" element = { <NotFound /> }/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
