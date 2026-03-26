f9fcee0d5b2ec4789b17b151127d53eebd55f569
Kevin Dodokal <dodokalkev@gmail.com>
2026-03-26 16:15:51 +0100

    refactor: sémantique article et correction largeur Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,15 @@
-import './Card.css'
-import { Link } from 'react-router-dom'
+import "./Card.css";
+import { Link } from "react-router-dom";
 
-function Card ({id, title, cover }) {
-    return (
-        <Link to= {`/logement/${id}`} className='card'>
-            <img src= {cover} alt= {title} className='card__image' />
-            <p className='card__title'> {title} </p>
-        </Link>
-    )
+function Card({ id, title, cover }) {
+  return (
+    <article>
+      <Link to={`/logement/${id}`} className="card">
+        <img src={cover} alt={title} className="card__image" />
+        <p className="card__title"> {title} </p>
+      </Link>
+    </article>
+  );
 }
 
-export default Card
+export default Card;
