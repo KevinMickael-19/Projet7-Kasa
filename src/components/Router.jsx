244be95df0a735367373ba33791f37d81ed1d1f9
Kevin Dodokal <dodokalkev@gmail.com>
2026-04-02 00:20:06 +0200

    feat: ajout page 404 avec responsive

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import About from "../pages/About/About";
 import Logement from "../pages/Logement/Logement";
-import NotFound from "../pages/NotFound";
+import NotFound from "../pages/NotFound/NotFound";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer"
 
---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import "./NotFound.css"
+
+function NotFound() {
+  return (
+    <section className="notfound">
+      <h1 className="notfound__title">404</h1>
+      <p className="notfound__message">
+        Oups ! La page que vous demandez n'existe pas.
+      </p>
+    <Link className="notfound__link"  to ="/"> Retournez sur la page d'Accueil</Link>
+    </section>
+  );
+}
+
+export default NotFound;
