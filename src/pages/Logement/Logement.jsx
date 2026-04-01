30582f66a0257dadb274a06bcabc7857f04c23d5
Kevin Dodokal <dodokalkev@gmail.com>
2026-04-01 16:27:57 +0200

    feat(logement): ajout rating et responsive zone info page Logement

--- src/pages/Logement/Logement.jsx
+++ src/pages/Logement/Logement.jsx
@@ -1,6 +1,8 @@
 import { useParams, Navigate } from "react-router-dom";
 import logements from "../../data/logements.json";
-import './Logement.css'
+import "./Logement.css";
+import starActive from "../../assets/images/star-active.svg"
+import starInactive from "../../assets/images/star-inactive.svg"
 
 function Logement() {
   const { id } = useParams();
@@ -8,6 +10,7 @@ function Logement() {
   if (!logement) {
     return <Navigate to="/404" />;
   }
+  const stars = [1, 2, 3, 4, 5]
   return (
     <section className="logement">
       <div className="logement__slideshow">
@@ -16,6 +19,37 @@ function Logement() {
           src={logement.cover}
           alt={logement.title}
         />
+      </div>
+      <div className="logement__info">
+        <div className="logement__info-left">
+          <h1 className="logement__title"> {logement.title} </h1>
+          <p className="logement__location"> {logement.location} </p>
+          <div className="logement__tags">
+            {logement.tags.map((tag) => (
+              <span key={tag} className="logement__tag">
+                {tag}
+              </span>
+            ))}
+          </div>
+        </div>
+        <div className="logement__info-right">
+          <div className="logement__host">
+            <span className="logement__host-name">
+              {logement.hôte.nom}
+            </span>
+            <img className="logement__host-img" src={logement.hôte.image} />
+          </div>
+          <div className="logement__rating">
+            {stars.map((star) => (
+              <img
+              key={star}
+              src= {star <= Number(logement.note) ? starActive : starInactive}
+              alt="star"
+              className="logement__star"
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </section>
   );
